61987b894b6dd2a5cebd08cb96b830e117a505a0
root <iamlamb1988@gmail.com>
2023-08-25 02:36:35 +0000

    Set up minimum node.js express page

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// Express webserver Listener setup
 const express=require('express');
 const app=express();
 app.listen(3000);
 
+//"get" represents HTTP GET request
+//
 app.get('/', (req, res) => {
-	res.send("<h1>Hello Express</h1>"); //home page
+	// console.log(req); //prints request object from client browser (or other request point). Very big object
+	// console.log(req.socket); //prints many details from socket
+	// console.log(req.socket.localAddress);
+	res.send("<h1>Hello Express</h1><p>IP: "+req.socket.localAddress+"</p>");
 });
